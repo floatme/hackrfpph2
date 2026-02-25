1da5624f99a84e520de7e4f92c2e1714d7b01bc5
floatme <34751963+floatme@users.noreply.github.com>
2026-02-25 23:14:38 +0200

    release: prepare 0.4.0-beta.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.orbit.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.4.0-beta.3"
+        versionCode = 7
+        versionName = "0.4.0-beta.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
